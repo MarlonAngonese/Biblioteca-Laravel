2c7ba97a0c31b0f374da2cf1560b7e8d8cdea875
MarlonAngonese <marlonmaster2020@gmail.com>
2022-06-26 20:45:54 -0300

    Set all tables relationships

--- app/Models/AuthorBook.php
+++ app/Models/AuthorBook.php
@@ -6,4 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AuthorBook extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'book_id',
+        'author_id'
+    ];
 }
